ddf80e65526e84353d6649b7f98454f61ccefaf7
Florian Gubler <florian.gubler@ergon.ch>
2025-01-29 08:20:57 +0100

    Added necessary imports
    Sorry, changes outside of IDEs are dangerous

--- demos/angular-capacitor/src/app/app.module.ts
+++ demos/angular-capacitor/src/app/app.module.ts
@@ -1,13 +1,13 @@
-import { NgModule } from '@angular/core';
+import { NgModule, APP_INITIALIZER } from '@angular/core';
 import { RouteReuseStrategy } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { CoreModule } from './core/core.module';
+import {AuthService} from "../../../../lib";
 
 @NgModule({
   declarations: [AppComponent],
